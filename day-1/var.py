cf39122ecee1fbb7a175433cfeb6b45021eddd83
Shivang Kushwaha <shivangkushwaha025@gmail.com>
2024-02-14 23:01:06 +0530

    Added Day-1 Exersise

---
+++ day-1/var.py
@@ -0,0 +1,25 @@
+# Here we will learn for python variables 
+x = 5
+print("It is an integer variable:- ", x)
+
+x= 'i am a string'
+print("Now it is string:- ",x)
+
+x= 4.5
+print("Now it is a Floting Point Number:-", x)
+
+# now we will see type casting 
+
+x= int(x)
+print("X- ", type(x))
+
+x= str(x)
+print("X- ", type(x))
+
+x= float(x)
+print("X- ", type(x))
+
+# Case sensitivity 
+a = 4
+A = "Sally"
+# A will not overwrite a
---
+++ day-1/var2.py
@@ -0,0 +1,18 @@
+# Variable with assign multiple valus
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+# You can also use the + operator to output multiple variables:
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+# For numbers, the + character works as a mathematical operator:
+
+x = 5
+y = 10
+print(x + y)
