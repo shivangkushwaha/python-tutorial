5e8d31e965955168635763cbae937412dde1061b
Shivang Kushwaha <shivangkushwaha025@gmail.com>
2024-02-16 22:40:51 +0530

    Added day-3 collection tutorial about casting of variables

---
+++ day-3/casting.py
@@ -0,0 +1,23 @@
+#  Let's see how we type cast a variable type   
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+print(x,type(x))
+print(y,type(y))
+print(z,type(z))
+
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+print(x,type(x))
+print(y,type(y))
+print(z,type(z))
+print(w,type(z))
+
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
+print(x,type(x))
+print(y,type(y))
+print(z,type(z))
