593b341b5ae3d35416c87e6f7341e18ed0c12b18
Shivang Kushwaha <shivangkushwaha025@gmail.com>
2024-02-20 20:43:28 +0530

    Added Bool statement

---
+++ day-6/bool.py
@@ -0,0 +1,33 @@
+# In programming you often need to know if an expression is True or False.
+# You can evaluate any expression in Python, and get one of two answers, True or False.
+# When you compare two values, the expression is evaluated and Python returns the Boolean answer
+
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+# When you run a condition in an if statement, Python returns True or False:
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+bool("abc")
+bool(123)
+bool(["apple", "cherry", "banana"])
+
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
+
+def myFunction() :
+  return True
+
+print(myFunction())
