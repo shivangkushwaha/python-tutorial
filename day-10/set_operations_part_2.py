62d010a5288dff8192996f413bc3ecc5bf358ec3
Shivang Kushwaha <shivangkushwaha025@gmail.com>
2024-02-28 21:19:19 +0530

    Added Set operations part-2

---
+++ day-10/set_operations_part_2.py
@@ -0,0 +1,41 @@
+"""Loop Items
+You can loop through the set items by using a for loop:"""
+
+# Loop through the set, and print the values:
+thisset = {"apple", "banana", "cherry"}
+for x in thisset:
+  print(x)
+
+# The union() method returns a new set with all items from both sets:
+set1 = {"a", "b" , "c"}
+set2 = {1, 2, 3}
+set3 = set1.union(set2)
+print(set3)
+
+# The update() method inserts the items in set2 into set1:
+set1 = {"a", "b" , "c"}
+set2 = {1, 2, 3}
+set1.update(set2)
+print(set1)
+
+# Keep ONLY the Duplicates
+# Keep the items that exist in both set x, and set y:
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+x.intersection_update(y)
+print(x)
+
+
+# Return a set that contains the items that exist in both set x, and set y:
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+z = x.intersection(y)
+print(z)
+
+
+# The symmetric_difference_update() method will keep only the elements that are NOT present in both sets.
+# Keep the items that are not present in both sets:
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+x.symmetric_difference_update(y)
+print(x)
