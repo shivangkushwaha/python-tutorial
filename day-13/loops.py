a0ecab27b05f30a924da8297defd8e4eecf800ce
Shivang Kushwaha <shivangkushwaha025@gmail.com>
2024-03-05 21:22:50 +0530

    Added loops while and for

---
+++ day-13/loops.py
@@ -0,0 +1,110 @@
+"""_summary_
+    Python Loops
+    Python has two primitive loop commands:
+
+    while loops
+    for loops
+"""
+# With the while loop we can execute a set of statements as long as a condition is true.
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+
+# The while loop requires relevant variables to be ready, in this example we need to define an indexing variable, i, which we set to 1.
+# With the break statement we can stop the loop even if the while condition is true:
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+
+# With the continue statement we can stop the current iteration, and continue with the next:
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+
+# With the else statement we can run a block of code once when the condition no longer is true:
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
+
+"""
+    _summary_
+    Python For Loops
+A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
+This is less like the for keyword in other programming languages, and works more like an iterator method as found in 
+other object-orientated programming languages.
+With the for loop we can execute a set of statements, once for each item in a list, tuple, set etc.
+
+"""
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  
+# Even strings are iterable objects, they contain a sequence of characters:
+for x in "banana":
+  print(x)
+
+# With the break statement we can stop the loop before it has looped through all the items:
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
+
+# Exit the loop when x is "banana", but this time the break comes before the print:
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
+
+# With the continue statement we can stop the current iteration of the loop, and continue with the next:
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
+
+# To loop through a set of code a specified number of times, we can use the range() function,
+# The range() function returns a sequence of numbers, starting from 0 by default, and increments by 1 (by default), and ends at a specified number.
+for x in range(6):
+  print(x)
+
+for x in range(2, 6):
+  print(x)
+
+for x in range(2, 30, 3):
+  print(x)
+
+for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")
+
+for x in range(6):
+  if x == 3: break
+  print(x)
+else:
+  print("Finally finished!")
+
+
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
+
+
+for x in [0, 1, 2]:
+  pass
